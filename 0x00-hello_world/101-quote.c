564f6a6a0cc8863b096a3b435d77d881696f7b19
Kukoyi Gbolahan <kukoyimustapha@gmail.com>
2023-02-20 10:44:04 -0800

    no 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - return a particular output
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
