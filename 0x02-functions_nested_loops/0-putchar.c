efde6d3d2aaa01d5094ac608a2a0dd25f367b43d
Kukoyi Gbolahan <kukoyimustapha@gmail.com>
2023-02-22 07:28:32 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,20 +3,19 @@
 /**
  * main - print _putchar
  *
- * Return: Always 0
+ * Return: 0
  */
 
 int main(void)
 {
-	putchar("_");
-	putchar("p");
-	putchar("u");
-	putchar("t");
-	putchar("c");
-	putchar("h");
-	putchar("a");
-	putchar("r");
-	putchar("\n");
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
